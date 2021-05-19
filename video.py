8692d4b0ba6b30e3607cf736cba8aee0c3b94d43
Camila <59348721+Catherine-Tranquil@users.noreply.github.com>
2021-05-19 19:57:11 +0800

    Update video.py

--- video.py
+++ video.py
@@ -1,9 +1,3 @@
-#-------------------------------------#
-#   调用摄像头或者视频进行检测
-#   调用摄像头直接运行即可
-#   调用视频可以将cv2.VideoCapture()指定路径
-#   视频的保存并不难，可以百度一下看看
-#-------------------------------------#
 import time
 
 import cv2
@@ -15,14 +9,20 @@ from yolo import YOLO
 yolo = YOLO()
 #-------------------------------------#
 #   调用摄像头
-#   capture=cv2.VideoCapture("1.mp4")
+capture=cv2.VideoCapture("drive.mp4")
 #-------------------------------------#
-capture=cv2.VideoCapture(0)
+#capture=cv2.VideoCapture(0)
 fps = 0.0
-while(True):
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+fps = int(capture.get(cv2.CAP_PROP_FPS))
+video = cv2.VideoWriter('result.mp4', fourcc, fps, (1920, 1080))
+
+while(capture.isOpened()):
     t1 = time.time()
     # 读取某一帧
-    ref,frame=capture.read()
+    ret,frame=capture.read()
+    if not ret:
+        break
     # 格式转变，BGRtoRGB
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     # 转变成Image
@@ -36,9 +36,15 @@ while(True):
     print("fps= %.2f"%(fps))
     frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow("video",frame)
+    # cv2.imshow("video",frame)
 
-    c= cv2.waitKey(1) & 0xff 
+    
+    video.write(frame)
+
+    #if cv2.waitKey(int(fps)) == 27:
+    #    break
+    c= cv2.waitKey(1) & 0xff
     if c==27:
-        capture.release()
-        break
+      break
+capture.release()
+video.release()
